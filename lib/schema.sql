8a6b2d85f4cea2daf68e0d34791c90c37c056f0f
Joel Lehtonen <joel.lehtonen@iki.fi>
2021-01-02 18:32:14 +0100

    Add "neighbour" field to the database, helping to find related callsigns (may be restricted to allocate them)

--- lib/schema.sql
+++ lib/schema.sql
@@ -3,18 +3,23 @@ BEGIN;
 
 CREATE TABLE event (
 	callsign TEXT NOT NULL,  -- Callsign, e.g. OH6DDR
+	neighbour TEXT NOT NULL, -- For finding the neighbours
 	status TEXT NOT NULL,    -- VARAUS / VOIMASSA / KARENSSI
 	from_date TEXT NOT NULL, -- Start date of this status
 	to_date TEXT NOT NULL    -- End date of this status
 );
 
+-- Used by the engine itself
 CREATE INDEX ix_callsign_date ON event(callsign, from_date);
 CREATE INDEX ix_status_callsign ON event(status, callsign, to_date);
 CREATE INDEX ix_anomaly ON event(to_date, status);
 
+-- Useful indices for common operations
+CREATE INDEX ix_neighbour ON event(neighbour, to_date);
+
 CREATE TABLE updates (
-	hash TEXT NOT NULL,
-	authored TEXT NOT NULL
+	hash TEXT NOT NULL,    -- Commit hash of the operation
+	authored TEXT NOT NULL -- Date of the data
 );
 
 -- Genesis commit
