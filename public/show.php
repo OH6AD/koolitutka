e74b33eb709efb1be3d9df58f2322957a0e34ccf
Joel Lehtonen <joel.lehtonen@iki.fi>
2021-01-05 16:12:27 +0100

    Clean up output so it is ready for javascript consumption

--- public/show.php
+++ public/show.php
@@ -19,19 +19,50 @@ $db = new SQLite3($config->database);
 $db->busyTimeout(2000);
 $db->exec('PRAGMA journal_mode = wal');
 
-$query = $db->prepare("SELECT callsign, status, from_date, to_date FROM event WHERE neighbour=? ORDER BY from_date");
+$query = $db->prepare("SELECT callsign, status, from_date, to_date FROM event WHERE neighbour=? AND (callsign=neighbour OR to_date='NOW') ORDER BY from_date");
 
 $callsign = strtoupper($_GET['q'] ?? $argv[1] ?? '');
 $neighbour = neighbour($callsign);
 
+$out = [
+    'now'     => null,
+    'history' => [],
+    'related' => [],
+    'genesis' => $db->querySingle("SELECT authored FROM updates ORDER BY rowid ASC LIMIT 1 OFFSET 1"),
+    'current' => $db->querySingle("SELECT authored FROM updates ORDER BY rowid DESC LIMIT 1"),
+];
+$history_end = null;
+
 $result = db_execute($query, [$neighbour]);
 
-$out = [
-    'raw' => [],
-];
-
 while (($row = $result->fetchArray(SQLITE3_ASSOC)) !== FALSE) {
-    array_push($out['raw'], $row);
+    // Nullify genesis and current to make it more jsonish
+    if ($row['from_date'] === $out['genesis']) {
+        unset($row['from_date']);
+    }
+    if ($row['to_date'] === 'NOW') {
+        unset($row['to_date']);
+    }
+
+    if ($row['callsign'] === $callsign || $row['callsign'] === $neighbour) {        
+        // Related to our callsign directly
+        if (array_key_exists('to_date', $row)) {
+            array_push($out['history'], $row);
+            $history_end = $row['to_date'];
+        } else {
+            $out['now'] = $row;
+        }
+    } else {
+        array_push($out['related'], $row);
+    }
+}
+
+if ($out['now'] === null) {
+    $out['now'] = [
+        'callsign' => $callsign,
+        'from_date' => $history_end,
+        'status' => 'VAPAA',
+    ];
 }
 
 header('Content-Type: application/json; charset=UTF-8');
